e1ac937d724cfc460ac1a7dc584628369a42f4e2
Mohamed16007163 <mohamed.elseidy@student.giu-uni.de>
2025-11-12 16:01:28 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,99 +6,125 @@
 #include "Visitor.h"
 #include <iostream>
 using namespace std;
-Animal::Animal(string name, int age, bool isHung){
-    this->name=name;
-    this->age=age;
-    this->isHung=isHung;
+
+
+Animal::Animal(string name, int age, bool isHung) {
+    this->name = name;
+    this->age = age;
+    this->isHung = isHung;
 }
-void Animal::feed(){
-    if(isHung==true){
-        cout<< "Hungry"<< endl;
+
+void Animal::feed() {
+    if (isHung)
+        cout << "Hungry" << endl;
+    else
+        cout << "Not Hungry" << endl;
+}
+
+void Animal::display() {
+    cout << "Name of the animal: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Hungry: " << (isHung ? "Yes" : "No") << endl;
+}
+
+Mammel::Mammel(string furColor, string name, int age, bool isHung)
+    : Animal(name, age, isHung) {
+    this->furColor = furColor;
+}
+
+void Mammel::display() {
+    Animal::display();
+    cout << "It's fur color is: " << furColor << endl;
+}
+
+Bird::Bird(float WingSpan, string name, int age, bool isHung)
+    : Animal(name, age, isHung) {
+    this->WingSpan = WingSpan;
+}
+
+void Bird::display() {
+    Animal::display();
+    cout << "The wing span is: " << WingSpan << endl;
+}
+
+
+Reptile::Reptile(bool isVenomus, string name, int age, bool isHung)
+    : Animal(name, age, isHung) {
+    this->isVenomus = isVenomus;
+}
+
+void Reptile::display() {
+    Animal::display();
+    cout << "This reptile is " << (isVenomus ? "Venomous" : "Not venomous") << endl;
+}
+
+
+Enclosure::Enclosure(Animal* animals, int capacity, int CurrentCount) {
+    this->animals = animals;
+    this->capacity = capacity;
+    this->CurrentCount = CurrentCount;
+}
+
+void Enclosure::addAnimel(Animal* a) {
+    if (CurrentCount < capacity) {
+        animals[CurrentCount++] = *a;
+        cout << "Animal added to the enclosure." << endl;
+    } else {
+        cout << "Enclosure is full!" << endl;
     }
-    cout<< "Not Hungry"<<endl;
 }
 
-void Animal:: display(){
-    cout<< "name of the animal: "<< name<< endl;
-    cout<< "the age: "<< age<< endl;
-    cout<< "the animal is: "<< isHung<< endl;
-    
+Enclosure::~Enclosure() {
+    delete[] animals;
 }
 
-Mammel::Mammel(string furColor, string name, int age, bool isHung): Animal(name, age, isHung){
-    this->furColor=furColor;
-}
-void Mammel:: display(){
-    Animal::display();
-    cout<< "it's furColor is: "<< furColor<< endl;
-    
-}
-Bird::Bird(float WingSpan, string name, int age, bool isHung): Animal(name, age, isHung){
-    this->WingSpan=WingSpan;
-}
-void Bird:: display(){
-    Animal::display();
-    cout<< "the WingSpan is: "<< WingSpan<< endl;
-}
-Reptile::Reptile(bool isVenomus, string name, int age, bool isHung): Animal(name, age, isHung){
-    isVenomus=isVenomus;
-}
-
-void Reptile::display(){
-    Animal::display();
-    cout<< " this Reptile is: " <<isVenomus<< endl ;
-}
-Enclosure:: Enclosure(Animal * Animal, int capacity, int CurrentCount){
-   this-> Animal= Animal;
-   this->capacity=capacity;
-   this->CurrentCount=CurrentCount;
-}
-void Enclosure:: addAnimel(Animal *a){
-    a=new Animal[CurrentCount + 1];
-    for(int i=0;i<CurrentCount;i++){
-        if(a[i]==null){
-            
-        }
+void Enclosure::diplayAnimals() {
+    cout << "--- Animals in Enclosure ---" << endl;
+    for (int i = 0; i < CurrentCount; i++) {
+        animals[i].display();
+        
     }
 }
-~Enclosure(){
-    delete [] Animal;
+
+int Enclosure::getCapacity(int c) {
+    capacity += c;
+    return capacity;
 }
-void Enclosure::  diplayAnimals(){
-    cout<< Animal->display();
-    
-}
-int Enclosure::   getCapacity(int c){
-   return  capacity=+c;
-}
-int Enclosure::  getCurrentCount(int count){
-    
-    return CurrentCount+=count;
-}
-Visitor::Visitor(String name,int numTickets){
-    this->name=name;
-    this->numTickets=numTickets;
-}
-void Visitor::  displayinfo(){
-    cout<<"name of the visitor: "<< name<<endl;
-    cout<< "number of tickets that was bought: "<< numTickets<<endl;
-}
-int Visitor::  getTickets(int ticketsBought){
-    return numTickets+=ticketsBought;
+
+int Enclosure::getCurrentCount(int count) {
+    CurrentCount += count;
+    return CurrentCount;
 }
 
 
-int main(){
-    Mammel Lion("red", "lion", 5, true);
-    Bird Parrot(3.9, "PArrot", 2, false);
-    Reptile Snake(true, "Sanke", 3, true);
-    Lion.diplay();
+Visitor::Visitor(string name, int numTickets) {
+    this->name = name;
+    this->numTickets = numTickets;
+}
+
+void Visitor::displayinfo() {
+    cout << "Name of the visitor: " << name << endl;
+    cout << "Number of tickets bought: " << numTickets << endl;
+}
+
+int Visitor::getTickets(int ticketsBought) {
+    numTickets += ticketsBought;
+    return numTickets;
+}
+
+
+int main() {
+    Mammel Lion("Red", "Lion", 5, true);
+    Bird Parrot(3.9, "Parrot", 2, false);
+    Reptile Snake(true, "Snake", 3, true);
+
+    Lion.display();
     Parrot.display();
-    Sanke.display();
-    
-    Visitor x("Sara ALi", 3)
+    Snake.display();
+
+    Visitor x("Sara Ali", 3);
     x.getTickets(3);
     x.displayinfo();
-    
+
     return 0;
 }
